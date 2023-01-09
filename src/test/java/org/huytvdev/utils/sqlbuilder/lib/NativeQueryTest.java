e773158ca6f4643775f96669ea02cdc921030dff
huy8895 <huytv.ous@mcredit.com.vn>
2023-01-09 13:39:34 +0700

    [query builder] add test TDD

---
+++ src/test/java/org/huytvdev/utils/sqlbuilder/lib/NativeQueryTest.java
@@ -0,0 +1,13 @@
+package org.huytvdev.utils.sqlbuilder.lib;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NativeQueryTest {
+    @Test
+    void test(){
+
+    }
+
+}
